1bdd58140c04f9aa595f6f10e7b6253f841f1b78
q_kul <q_kul@mail.ru>
2018-07-22 23:23:40 +0300

    new models

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; } //the primary key, первичный ключ
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "it's a long word(max 50characters).")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
         //свойство навигации (содержит другие сущности)
     }
